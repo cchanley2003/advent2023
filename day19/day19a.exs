e4a950de2b1c48d9f1241d3adfe20cbdf28ef32a
CHAD HANLEY <chanley@caci.com>
2023-12-30 10:34:49 -0700

    More day19.

--- day19/day19a.exs
+++ day19/day19a.exs
@@ -1,9 +1,18 @@
 defmodule Day19a do
   def process(path) do
-    contents = File.read!(path)
-    String.split(contents, "\n\n")
+    [rules, input] = File.read!(path)
+    |> String.split("\n\n")
     |> Enum.map(fn line ->
       String.split(line, "\n")
+    end)
+
+    rules
+    |> Enum.map(fn line ->
+      # Capture "px{a<2006:qkq,m>2090:A,rfg}
+     tl(Regex.run(~r/(.*){(.*)}/, line))
+    end)
+    |> Enum.map(fn [rule_name, rules] ->
+      {rule_name, rules |> String.split(",")}
     end)
   end
 end
